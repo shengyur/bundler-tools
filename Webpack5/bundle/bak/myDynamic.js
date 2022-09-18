952710408a658c5425f5846161521ba1fca157ce
zhi <sheldo.cn@gmail.com>
2022-09-18 15:43:56 +0800

    feat: dynamic import done!!

--- Webpack5/bundle/bak/myDynamic.js
+++ Webpack5/bundle/bak/myDynamic.js
@@ -11,7 +11,6 @@ function __webpack_require__(moduleId) {
   __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
   return module.exports;
 }
-__webpack_require__.m = __webpack_modules__;
 
 __webpack_require__.d = (exports, definition) => {
   for (var key in definition) {
@@ -24,82 +23,14 @@ __webpack_require__.d = (exports, definition) => {
   }
 };
 
-
-__webpack_require__.f = {};
 __webpack_require__.e = chunkId => {
-  return Promise.all(Object.keys(__webpack_require__.f).reduce((promises, key) => {
-    __webpack_require__.f[key](chunkId, promises);
-    return promises;
-  }, []));
+  let promises = []
+  __webpack_require__.f.j(chunkId, promises)
+
+  return Promise.all(promises)
 };
-
-
-__webpack_require__.u = chunkId => {
-  return "" + chunkId + ".js";
-};
-
-
-__webpack_require__.g = function () {
-  if (typeof globalThis === 'object') return globalThis;
-  try {
-    return this || new Function('return this')();
-  } catch (e) {
-    if (typeof window === 'object') return window;
-  }
-}();
-
 
 __webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop);
-
-
-var inProgress = {};
-var dataWebpackPrefix = "bundle:";
-__webpack_require__.l = (url, done, key, chunkId) => {
-  if (inProgress[url]) {
-    inProgress[url].push(done);
-    return;
-  }
-  var script, needAttach;
-  if (key !== undefined) {
-    var scripts = document.getElementsByTagName("script");
-    for (var i = 0; i < scripts.length; i++) {
-      var s = scripts[i];
-      if (s.getAttribute("src") == url || s.getAttribute("data-webpack") == dataWebpackPrefix + key) {
-        script = s;
-        break;
-      }
-    }
-  }
-  if (!script) {
-    needAttach = true;
-    script = document.createElement('script');
-    script.charset = 'utf-8';
-    script.timeout = 120;
-    if (__webpack_require__.nc) {
-      script.setAttribute("nonce", __webpack_require__.nc);
-    }
-    script.setAttribute("data-webpack", dataWebpackPrefix + key);
-    script.src = url;
-  }
-  inProgress[url] = [done];
-  var onScriptComplete = (prev, event) => {
-    script.onerror = script.onload = null;
-    clearTimeout(timeout);
-    var doneFns = inProgress[url];
-    delete inProgress[url];
-    script.parentNode && script.parentNode.removeChild(script);
-    doneFns && doneFns.forEach(fn => fn(event));
-    if (prev) return prev(event);
-  };
-  var timeout = setTimeout(onScriptComplete.bind(null, undefined, {
-    type: 'timeout',
-    target: script
-  }), 120000);
-  script.onerror = onScriptComplete.bind(null, script.onerror);
-  script.onload = onScriptComplete.bind(null, script.onload);
-  needAttach && document.head.appendChild(script);
-};
-
 
 __webpack_require__.r = exports => {
   if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
@@ -112,81 +43,177 @@ __webpack_require__.r = exports => {
   });
 };
 
+var installedChunks = {
+  "index": 0,
+  // 加载中的模块，会被记录到这边，value值是数组时，说明还在加载，0表示加载完成了
+  // "src_dynamic_js": [resolve,reject,promise]
+};
+__webpack_require__.f = {}
+// publicPath 文件访问路径
+__webpack_require__.p = ''
+// 文件名
+__webpack_require__.u = (chunkId) => chunkId + '.js'
 
-var scriptUrl;
-if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
-var document = __webpack_require__.g.document;
-if (!scriptUrl && document) {
-  if (document.currentScript) scriptUrl = document.currentScript.src;
-  if (!scriptUrl) {
-    var scripts = document.getElementsByTagName("script");
-    if (scripts.length) scriptUrl = scripts[scripts.length - 1].src;
-  }
+// 加载脚本
+__webpack_require__.l = (url) => {
+  let script = document.createElement('script')
+  script.src = url
+  document.head.appendChild(script)
 }
-if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
-scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
-__webpack_require__.p = scriptUrl;
+// jsonp 通过 JSONP 的方式加载 chunkId 对应的 JS 文件，生成一个 promise 放到 promises 数组里
+__webpack_require__.f.j = (chunkId, promises) => {
+  let installedChunkData
 
+  const promise = new Promise((resolve, reject) => {
+    installedChunkData = installedChunks[chunkId] = [resolve, reject]
+  })
+  // 把当前模块promise的 resolve  reject 和 promise 本身存放到数组里
+  installedChunkData[2] = promise
+
+  promises.push(promise)
+
+  const url = __webpack_require__.p + __webpack_require__.u(chunkId)
+
+  __webpack_require__.l(url)
+};
+
+var webpackJsonpCallback = ([chunkIds, moreModules]) => {
+  const resolves = []
+
+  for (let i = 0; i < chunkIds.length; i++) {
+    const chunkId = chunkIds[i]
+
+    resolves.push(installedChunks[chunkId][0])
+    installedChunks[chunkId] = 0
+  }
+
+  // 合并模块定义
+  for (const moduleId in moreModules) {
+    __webpack_modules__[moduleId] = moreModules[moduleId]
+  }
+  // 依次取出 promise 的 resolve 方法，让它对应的promise 变成success状态
+  while(resolves.length){
+    // 按队列顺序  先加载的先执行
+    const firstDone = resolves.shift()
+    firstDone()
+  }
+
+};
+// 全局变量  用来保存  webpackJsonpCallback  以与新模块共享
+var chunkLoadingGlobal = self["webpackChunkbundle"] = self["webpackChunkbundle"] || [];
+chunkLoadingGlobal.push = webpackJsonpCallback
+
+var __webpack_exports__ = {};
+__webpack_require__.e("src_dynamic_js").then(__webpack_require__.bind(__webpack_require__, "./src/dynamic.js")).then(result => {
+  console.log('我是dym 1', result.default);
+});
+var __webpack_modules__ = {};
+var __webpack_module_cache__ = {};
+function __webpack_require__(moduleId) {
+  var cachedModule = __webpack_module_cache__[moduleId];
+  if (cachedModule !== undefined) {
+    return cachedModule.exports;
+  }
+  var module = __webpack_module_cache__[moduleId] = {
+    exports: {}
+  };
+  __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+  return module.exports;
+}
+
+__webpack_require__.d = (exports, definition) => {
+  for (var key in definition) {
+    if (__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+      Object.defineProperty(exports, key, {
+        enumerable: true,
+        get: definition[key]
+      });
+    }
+  }
+};
+
+__webpack_require__.e = chunkId => {
+  let promises = []
+  __webpack_require__.f.j(chunkId, promises)
+
+  return Promise.all(promises)
+};
+__webpack_require__.g = function () {
+
+}();
+
+__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop);
+
+__webpack_require__.r = exports => {
+  if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+    Object.defineProperty(exports, Symbol.toStringTag, {
+      value: 'Module'
+    });
+  }
+  Object.defineProperty(exports, '__esModule', {
+    value: true
+  });
+};
 
 var installedChunks = {
-  "index": 0
+  "index": 0,
+  // 加载中的模块，会被记录到这边，value值是数组时，说明还在加载，0表示加载完成了
+  // "src_dynamic_js": [resolve,reject,promise]
 };
+__webpack_require__.f = {}
+// publicPath 文件访问路径
+__webpack_require__.p = ''
+// 文件名
+__webpack_require__.u = (chunkId) => chunkId + '.js'
+
+// 加载脚本
+__webpack_require__.l = (url) => {
+  let script = document.createElement('script')
+  script.src = url
+  document.head.appendChild(script)
+}
+// jsonp 通过 JSONP 的方式加载 chunkId 对应的 JS 文件，生成一个 promise 放到 promises 数组里
 __webpack_require__.f.j = (chunkId, promises) => {
-  var installedChunkData = __webpack_require__.o(installedChunks, chunkId) ? installedChunks[chunkId] : undefined;
-  if (installedChunkData !== 0) {
-    if (installedChunkData) {
-      promises.push(installedChunkData[2]);
-    } else {
-      if (true) {
-        var promise = new Promise((resolve, reject) => installedChunkData = installedChunks[chunkId] = [resolve, reject]);
-        promises.push(installedChunkData[2] = promise);
-        var url = __webpack_require__.p + __webpack_require__.u(chunkId);
-        var error = new Error();
-        var loadingEnded = event => {
-          if (__webpack_require__.o(installedChunks, chunkId)) {
-            installedChunkData = installedChunks[chunkId];
-            if (installedChunkData !== 0) installedChunks[chunkId] = undefined;
-            if (installedChunkData) {
-              var errorType = event && (event.type === 'load' ? 'missing' : event.type);
-              var realSrc = event && event.target && event.target.src;
-              error.message = 'Loading chunk ' + chunkId + ' failed.\n(' + errorType + ': ' + realSrc + ')';
-              error.name = 'ChunkLoadError';
-              error.type = errorType;
-              error.request = realSrc;
-              installedChunkData[1](error);
-            }
-          }
-        };
-        __webpack_require__.l(url, loadingEnded, "chunk-" + chunkId, chunkId);
-      } else installedChunks[chunkId] = 0;
-    }
-  }
+  let installedChunkData
+
+  const promise = new Promise((resolve, reject) => {
+    installedChunkData = installedChunks[chunkId] = [resolve, reject]
+  })
+  // 把当前模块promise的 resolve  reject 和 promise 本身存放到数组里
+  installedChunkData[2] = promise
+
+  promises.push(promise)
+
+  const url = __webpack_require__.p + __webpack_require__.u(chunkId)
+
+  __webpack_require__.l(url)
 };
-var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
-  var [chunkIds, moreModules, runtime] = data;
-  var moduleId,
-    chunkId,
-    i = 0;
-  if (chunkIds.some(id => installedChunks[id] !== 0)) {
-    for (moduleId in moreModules) {
-      if (__webpack_require__.o(moreModules, moduleId)) {
-        __webpack_require__.m[moduleId] = moreModules[moduleId];
-      }
-    }
-    if (runtime) var result = runtime(__webpack_require__);
+
+var webpackJsonpCallback = ([chunkIds, moreModules]) => {
+  const resolves = []
+
+  for (let i = 0; i < chunkIds.length; i++) {
+    const chunkId = chunkIds[i]
+
+    resolves.push(installedChunks[chunkId][0])
+    installedChunks[chunkId] = 0
   }
-  if (parentChunkLoadingFunction) parentChunkLoadingFunction(data);
-  for (; i < chunkIds.length; i++) {
-    chunkId = chunkIds[i];
-    if (__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
-      installedChunks[chunkId][0]();
-    }
-    installedChunks[chunkId] = 0;
+
+  // 合并模块定义
+  for (const moduleId in moreModules) {
+    __webpack_modules__[moduleId] = moreModules[moduleId]
   }
+  // 依次取出 promise 的 resolve 方法，让它对应的promise 变成success状态
+  while(resolves.length){
+    // 按队列顺序  先加载的先执行
+    const firstDone = resolves.shift()
+    firstDone()
+  }
+
 };
+// 全局变量  用来保存  webpackJsonpCallback  以与新模块共享
 var chunkLoadingGlobal = self["webpackChunkbundle"] = self["webpackChunkbundle"] || [];
-chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
-chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+chunkLoadingGlobal.push = webpackJsonpCallback
 
 var __webpack_exports__ = {};
 __webpack_require__.e("src_dynamic_js").then(__webpack_require__.bind(__webpack_require__, "./src/dynamic.js")).then(result => {
--- Webpack5/bundle/dist/index.js
+++ Webpack5/bundle/dist/index.js
@@ -11,7 +11,6 @@ function __webpack_require__(moduleId) {
   __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
   return module.exports;
 }
-__webpack_require__.m = __webpack_modules__;
 
 __webpack_require__.d = (exports, definition) => {
   for (var key in definition) {
@@ -24,35 +23,17 @@ __webpack_require__.d = (exports, definition) => {
   }
 };
 
-
-__webpack_require__.f = {};
 __webpack_require__.e = chunkId => {
   let promises = []
-  __webpack_require__.f.j(chunkId,promises)
+  __webpack_require__.f.j(chunkId, promises)
 
   return Promise.all(promises)
 };
-
-
-__webpack_require__.u = chunkId => {
-  // TODO
-};
-
-
 __webpack_require__.g = function () {
-  
+
 }();
 
-
 __webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop);
-
-
-var inProgress = {};
-var dataWebpackPrefix = "bundle:";
-__webpack_require__.l = (url, done, key, chunkId) => {
-
-};
-
 
 __webpack_require__.r = exports => {
   if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
@@ -65,42 +46,65 @@ __webpack_require__.r = exports => {
   });
 };
 
-
-var scriptUrl;
-if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
-var document = __webpack_require__.g.document;
-if (!scriptUrl && document) {
-  if (document.currentScript) scriptUrl = document.currentScript.src;
-  if (!scriptUrl) {
-    var scripts = document.getElementsByTagName("script");
-    if (scripts.length) scriptUrl = scripts[scripts.length - 1].src;
-  }
-}
-if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
-scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
-__webpack_require__.p = scriptUrl;
-
-
-
 var installedChunks = {
-  "index": 0
+  "index": 0,
+  // 加载中的模块，会被记录到这边，value值是数组时，说明还在加载，0表示加载完成了
+  // "src_dynamic_js": [resolve,reject,promise]
 };
+__webpack_require__.f = {}
+// publicPath 文件访问路径
+__webpack_require__.p = ''
+// 文件名
+__webpack_require__.u = (chunkId) => chunkId + '.js'
+
+// 加载脚本
+__webpack_require__.l = (url) => {
+  let script = document.createElement('script')
+  script.src = url
+  document.head.appendChild(script)
+}
 // jsonp 通过 JSONP 的方式加载 chunkId 对应的 JS 文件，生成一个 promise 放到 promises 数组里
 __webpack_require__.f.j = (chunkId, promises) => {
   let installedChunkData
 
-  const promise = new Promise((resolve, reject)=> {
-    installedChunkData = installedChunks[chunkId] = [resolve,reject]
+  const promise = new Promise((resolve, reject) => {
+    installedChunkData = installedChunks[chunkId] = [resolve, reject]
   })
   // 把当前模块promise的 resolve  reject 和 promise 本身存放到数组里
   installedChunkData[2] = promise
+
+  promises.push(promise)
+
+  const url = __webpack_require__.p + __webpack_require__.u(chunkId)
+
+  __webpack_require__.l(url)
 };
-var webpackJsonpCallback = (parentChunkLoadingFunction, data) => {
-  
+
+var webpackJsonpCallback = ([chunkIds, moreModules]) => {
+  const resolves = []
+
+  for (let i = 0; i < chunkIds.length; i++) {
+    const chunkId = chunkIds[i]
+
+    resolves.push(installedChunks[chunkId][0])
+    installedChunks[chunkId] = 0
+  }
+
+  // 合并模块定义
+  for (const moduleId in moreModules) {
+    __webpack_modules__[moduleId] = moreModules[moduleId]
+  }
+  // 依次取出 promise 的 resolve 方法，让它对应的promise 变成success状态
+  while(resolves.length){
+    // 按队列顺序  先加载的先执行
+    const firstDone = resolves.shift()
+    firstDone()
+  }
+
 };
+// 全局变量  用来保存  webpackJsonpCallback  以与新模块共享
 var chunkLoadingGlobal = self["webpackChunkbundle"] = self["webpackChunkbundle"] || [];
-chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
-chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+chunkLoadingGlobal.push = webpackJsonpCallback
 
 var __webpack_exports__ = {};
 __webpack_require__.e("src_dynamic_js").then(__webpack_require__.bind(__webpack_require__, "./src/dynamic.js")).then(result => {
